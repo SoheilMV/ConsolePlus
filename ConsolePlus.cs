632ebec7dcc5042375677c9e2f8ee43d8a5b41e9
SoheilMV <Soheil.8221@gmail.com>
2023-07-02 17:06:00 +0330

    Added ConsolePlus.cs and README.md

---
+++ ConsolePlus.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using Console = Colorful.Console;
+
+public class ConsolePlus
+{
+    private static object _locker = new object();
+    private static Dictionary<string, int> _map = new Dictionary<string, int>();
+
+    public static void WriteLine(string name, object value, Color color = default)
+    {
+        lock (_locker)
+        {
+            _map.Add(name, Console.CursorTop);
+            Console.WriteLine(value, color == default ? Color.Gray : color);
+        }
+    }
+
+    public static void EditLine(string name, object value, Color color = default)
+    {
+        lock (_locker)
+        {
+            if (_map.ContainsKey(name))
+            {
+                int index = _map[name];
+                EditLine(index, value.ToString()!, color == default ? Color.Gray : color);
+            }
+        }
+    }
+
+    //https://stackoverflow.com/questions/34423986/c-sharp-rewrite-edit-line-while-program-is-running
+    private static void EditLine(int index, string value, Color color)
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, index);
+        Console.Write(value, color);
+        Console.WriteLine(new string(' ', Console.WindowWidth - value.Length), color);
+        Console.SetCursorPosition(0, currentLineCursor);
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# ConsolePlus
+Rewrite/Edit line in console application.
+
+# How To Use
+1. Install **Colorful.Console** from Manage Nuget Package.
+1. Copy and paste **ConsolePlus.cs** into your project.
+
+
+Now write a text in the console with the following command:
+```csharp
+ConsolePlus.WriteLine("myName", "My name is ...");
+```
+And you can edit the same line with the following command:
+```csharp
+ConsolePlus.EditLine("myName", "My name is Sohail MV");
+```
+
+
+# List of other open source libraries used
+- [Colorful.Console](https://github.com/tomakita/Colorful.Console) - Style your .NET console output
